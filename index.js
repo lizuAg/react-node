ec4de713a4547ec95d69b2daa5a3b0d0afd065d2
lizuAg <wndms058@gmail.com>
2021-11-04 02:30:46 +0900

    feat: 로그인 기능 구현 #12

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const port = 5000;
 const bodyParser = require("body-parser");
-
+const cookieParser = require("cookie-parser");
 const config = require("./config/key");
 
 const { User } = require("./models/User");
@@ -11,9 +11,11 @@ const { User } = require("./models/User");
 app.use(bodyParser.urlencoded({ extended: true }));
 //application/json
 app.use(bodyParser.json());
+app.use(cookieParser());
 
 //user pw 특수기호 없게 바꿔야함 !!!
 const mongoose = require("mongoose");
+const { restart } = require("nodemon");
 mongoose
   .connect(config.mongoURI, {})
   .then(() => console.log("MogoDB Connected..."))
@@ -33,6 +35,36 @@ app.post("/register", (req, res) => {
     return res.status(200).json({
       success: true,
     });
+  });
+});
+
+app.post("/login", (req, res) => {
+  //요청된 이메일을 데이터베이스에서 있는지 찾는다.
+  User.findOne({ email: req.body.email }, (err, userInfo) => {
+    if (!userInfo) {
+      return res.json({
+        loginSuccess: false,
+        message: "제공된 이메일에 해당하는 유저가 없습니다.",
+      });
+    }
+  });
+  //요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인
+  user.comparePassword(req.body.password, (err, isMatch) => {
+    if (!isMatch)
+      return res.json({
+        loginSuccess: false,
+        message: "비밀번호가 틀렸습니다.",
+      });
+  });
+  //비밀번호 맞으면 토큰 생성.
+  user.generateToken((err, user) => {
+    if (err) return res.status(400).send(err);
+
+    //토큰을 저장한다. 어디에? 쿠키(V), 로컬스토리지
+    res
+      .cookie("x_auth", user.token)
+      .status(200)
+      .json({ loginSuccess: true, userId: user._id });
   });
 });
 
