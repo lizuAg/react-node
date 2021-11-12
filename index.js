ea02b32cc4fc12d7d42f1a49c15d7afe80318feb
lizuAg <wndms058@gmail.com>
2021-11-13 01:35:20 +0900

    chore: 라우터 경로 수정

--- index.js
+++ index.js
@@ -25,7 +25,7 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.post("api/users/register", (req, res) => {
+app.post("/api/users/register", (req, res) => {
   //회원 가입할 때 필요한 정보들을 client에서 가져오면
   //그것들을 데이터 베이스에 넣어준다.
 
@@ -38,36 +38,44 @@ app.post("api/users/register", (req, res) => {
   });
 });
 
-app.post("api/users/login", (req, res) => {
+app.post("/api/users/login", (req, res) => {
+  // console.log('ping')
   //요청된 이메일을 데이터베이스에서 있는지 찾는다.
-  User.findOne({ email: req.body.email }, (err, userInfo) => {
-    if (!userInfo) {
+  User.findOne({ email: req.body.email }, (err, user) => {
+    // console.log('user', user)
+    if (!user) {
       return res.json({
         loginSuccess: false,
         message: "제공된 이메일에 해당하는 유저가 없습니다.",
       });
     }
-  });
-  //요청된 이메일이 DB에 있다면 비밀번호가 맞는지 확인
-  user.comparePassword(req.body.password, (err, isMatch) => {
-    if (!isMatch)
-      return res.json({
-        loginSuccess: false,
-        message: "비밀번호가 틀렸습니다.",
-      });
-  });
-  //비밀번호 맞으면 토큰 생성.
-  user.generateToken((err, user) => {
-    if (err) return res.status(400).send(err);
 
-    //토큰을 저장한다. 어디에? 쿠키(V), 로컬스토리지
-    res
-      .cookie("x_auth", user.token)
-      .status(200)
-      .json({ loginSuccess: true, userId: user._id });
+    //요청된 이메일이 데이터 베이스에 있다면 비밀번호가 맞는 비밀번호 인지 확인.
+    user.comparePassword(req.body.password, (err, isMatch) => {
+      if (!isMatch)
+        return res.json({
+          loginSuccess: false,
+          message: "비밀번호가 틀렸습니다.",
+        });
+
+      //비밀번호 까지 맞다면 토큰을 생성하기.
+      user.generateToken((err, user) => {
+        if (err) {
+          return res.status(400).send(err);
+        }
+
+        // 토큰을 저장한다.  어디에 ?  쿠키(V) , 로컬스토리지
+        res
+          .cookie("x_auth", user.token)
+          .status(200)
+          .json({ loginSuccess: true, userId: user._id });
+      });
+    });
   });
 });
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
+
+//app.get('api/users/auth',,(req,err)=>{})
