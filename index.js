04c914c22f8a433f21377131586e0bea104c6a8c
lizuAg <wndms058@gmail.com>
2021-11-13 01:55:39 +0900

    feat: auth 만들기 #13

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const config = require("./config/key");
-
+const { auth } = require("./middleware/auth");
 const { User } = require("./models/User");
 
 //application/x-www-form-urlencoded
@@ -74,8 +74,20 @@ app.post("/api/users/login", (req, res) => {
   });
 });
 
+app.get("api/users/auth", auth, (req, err) => {
+  //여기까지 미들웨어를 통과해 왔다는 얘기는 Authentication이 True라는 말.
+  res.status(200).json({
+    _id: req.user._id,
+    isAdmin: req.user.role === 0 ? false : true,
+    isAuth: true,
+    email: req.user.email,
+    name: req.user.name,
+    lastname: req.user.lastname,
+    role: req.user.role,
+    image: req.user.image,
+  });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-//app.get('api/users/auth',,(req,err)=>{})
