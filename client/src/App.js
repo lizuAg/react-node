7b5fbac07548dd9fe9da549cc221bf34a920dedb
lizuAg <wndms058@gmail.com>
2021-12-25 13:31:47 +0900

    chore: Switch-> Routes 변경

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import { BrowerRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import LadingPage from "./components/views/LandingPage/LadingPage";
 import LoginPage from "./components/views/LoginPage/LoginPage";
@@ -8,11 +8,11 @@ function App() {
   return (
     <Router>
       <div>
-        <Switch>
+        <Routes>
           <Route exact path="/" components={LadingPage} />
           <Route exact path="/login" components={LoginPage} />
           <Route exact path="/register" components={RegisterPage} />
-        </Switch>
+        </Routes>
       </div>
     </Router>
   );
